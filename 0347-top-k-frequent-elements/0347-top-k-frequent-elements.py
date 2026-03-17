798f7632037ba745f5e6fa0c46bc55aeb92df58f
Mouli91 <chandumathireddy2006@gmail.com>
2026-03-17 23:00:36 +0530

    Time: 0 ms (100%), Space: 23 MB (56.02%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -0,0 +1,7 @@
+from collections import Counter
+import heapq
+
+class Solution:
+    def topKFrequent(self, nums, k):
+        freq = Counter(nums)
+        return heapq.nlargest(k, freq.keys(), key=freq.get)
